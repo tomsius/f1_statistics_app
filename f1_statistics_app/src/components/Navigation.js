7630a3581116619525eeaaae437643a66a62eb18
Tomas Kaselynas <tomas.kaselynas@visma.com>
2021-02-20 14:47:52 +0200

    Add navigation bar component

---
+++ f1_statistics_app/src/components/Navigation.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
+import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
+
+export class Navigation extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <Navbar bg="dark" variant="dark" expand="lg">
+                <Navbar.Brand href="/">Pradžia</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mr-auto">
+                        <NavLink className="d-inline p-2 text-light" to="/wins">Laimėjimai</NavLink>
+                        <NavLink className="d-inline p-2 text-light" to="/wins">Pole pozicijos</NavLink>
+                    </Nav>
+                </Navbar.Collapse>
+        </Navbar>
+        );
+    }
+}
