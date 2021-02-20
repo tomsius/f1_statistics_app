92e5737facb4635963a5948863fd4f43b9a9653d
Tomas Kaselynas <tomas.kaselynas@visma.com>
2021-02-20 20:37:53 +0200

    Show chart

--- f1_statistics_app/src/components/Statistics/Wins/DriverWins.js
+++ f1_statistics_app/src/components/Statistics/Wins/DriverWins.js
@@ -1,5 +1,9 @@
-import React,{Component} from 'react';
-import {DataRangeForm} from '../../DataRangeForm';
+import React, { Component } from 'react';
+import { DataRangeForm } from '../../DataRangeForm';
+import CanvasJSReact from '../../../canvasjs.react';
+
+var CanvasJS = CanvasJSReact.CanvasJS;
+var CanvasJSChart = CanvasJSReact.CanvasJSChart;
 
 export class DriverWins extends Component {
     constructor(props) {
@@ -11,18 +15,49 @@ export class DriverWins extends Component {
         this.fillData = this.fillData.bind(this);
     }
 
-    fillData(data){
+    fillData(data) {
         this.setState({
             winners: data
         });
     }
 
     render() {
+        var data = this.state.winners.map(x => ({label: x.name, y: x.winCount}));
+
+        const options = {
+            data: [
+                {
+                    type: "column",
+                    dataPoints: data
+                }
+            ]
+        };
+
+        const options2 = {
+            data: [
+                {
+                    type: "pie",
+                    indexLabel: "{label} {y}",
+                    dataPoints: data
+                }
+            ]
+        };
+
         return (
             <div>
                 <h2>{this.props.pageTitle}</h2>
-                <br/>
-                <DataRangeForm api={this.props.api} callback={this.fillData}/>
+                <p style={{color:"red", textAlign:"center"}}>*Papildomai pridėti pie chart</p>
+                <br />
+                <DataRangeForm api={this.props.api} callback={this.fillData} />
+                <br />
+                {
+                    this.state.winners.length > 0 &&
+                    <div style={{position:"relative", right:"6em"}}>
+                        <CanvasJSChart options={options} />
+                        <br />
+                        <CanvasJSChart options={options2} />
+                    </div>
+                }
             </div>
         );
     }
