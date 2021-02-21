230d26a0ac150bf3ce720e3ebb5105555d79289f
Tomas Kaselynas <tomas.kaselynas@visma.com>
2021-02-21 17:49:24 +0200

    Add routes for poles subsystem

--- f1_statistics_app/src/App.js
+++ f1_statistics_app/src/App.js
@@ -8,6 +8,10 @@ import { Winners } from './components/Statistics/Wins/Winners';
 import { WinnersPercent } from './components/Statistics/Wins/WinnersPercent';
 import { WinnersUnique } from './components/Statistics/Wins/WinnersUnique';
 import { WinnersByTrack } from './components/Statistics/Wins/WinnersByTrack';
+import { Poles } from './components/Statistics/Poles/Poles';
+import { PoleSitters } from './components/Statistics/Poles/PoleSitters';
+import { PoleSittersUnique } from './components/Statistics/Poles/PoleSittersUnique';
+import { WinnersFromPole } from './components/Statistics/Poles/WinnersFromPole';
 import { NotFound } from './components/NotFound';
 
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
@@ -25,7 +29,6 @@ export default class App extends Component {
           <Route path='/' component={Home} exact />
 
           <Route path='/wins' component={Wins} exact />
-
           <Route path='/wins/drivers' exact render={(props) => (<Winners {...props} api={"wins/drivers"} pageTitle={"Lenktynininkų laimėjimai"} />)} />
           <Route path='/wins/constructors' exact render={(props) => (<Winners {...props} api={"wins/constructors"} pageTitle={"Komandų laimėjimai"} />)} />
           <Route path='/wins/driverspercent' exact render={(props) => (<WinnersPercent {...props} api={"wins/drivers/percent"} pageTitle={"Lenktynininkų laimėjimų procentas"} />)} />
@@ -33,6 +36,13 @@ export default class App extends Component {
           <Route path='/wins/driversunique' exact render={(props) => (<WinnersUnique {...props} api={"wins/drivers/uniqueperseason"} pageTitle={"Skirtingų lenktynininkų, laimėjusių lenktynes, skaičius"} />)} />
           <Route path='/wins/constructorsunique' exact render={(props) => (<WinnersUnique {...props} api={"wins/constructors/uniqueperseason"} pageTitle={"Skirtingų komandų, laimėjusių lenktynes, skaičius"} />)} />
           <Route path='/wins/driversbytrack' exact render={(props) => (<WinnersByTrack {...props} api={"wins/drivers/tracks"} pageTitle={"Lenktynininkų laimėjimai pagal trasą"} />)} />
+
+          <Route path='/poles' component={Poles} exact />
+          <Route path='/poles/drivers' exact render={(props) => (<PoleSitters {...props} api={"poles/drivers"} pageTitle={"Lenktynininkų pole pozicijos"} />)} />
+          <Route path='/poles/constructors' exact render={(props) => (<PoleSitters {...props} api={"poles/constructors"} pageTitle={"Komandų pole pozicijos"} />)} />
+          <Route path='/poles/driversunique' exact render={(props) => (<PoleSittersUnique {...props} api={"poles/drivers/uniqueperseason"} pageTitle={"Skirtingų lenktynininkų, laimėjusių pole poziciją, skaičius"} />)} />
+          <Route path='/poles/constructorsunique' exact render={(props) => (<PoleSittersUnique {...props} api={"poles/constructors/uniqueperseason"} pageTitle={"Skirtingų komandų, laimėjusių pole poziciją, skaičius"} />)} />
+          <Route path='/poles/winners' exact render={(props) => (<WinnersFromPole {...props} api={"poles/winners"} pageTitle={"Lenktynininkai, kurie laimėjo lenktynes iš pole pozicijos"} />)} />
 
           <Route path="*" component={NotFound} />
         </Switch>
