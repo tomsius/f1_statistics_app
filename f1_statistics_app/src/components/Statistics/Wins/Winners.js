3b3554380ddfafd05fe9f6c47b8ff2f6a49a2e79
Tomas Kaselynas <tomas.kaselynas@visma.com>
2021-02-21 11:13:48 +0200

    Change between charts

--- f1_statistics_app/src/components/Statistics/Wins/Winners.js
+++ f1_statistics_app/src/components/Statistics/Wins/Winners.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { DataRangeForm } from '../../DataRangeForm';
 import CanvasJSReact from '../../../canvasjs.react';
+import { Button } from 'react-bootstrap';
 
 var CanvasJS = CanvasJSReact.CanvasJS;
 var CanvasJSChart = CanvasJSReact.CanvasJSChart;
@@ -9,10 +10,13 @@ export class Winners extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            winners: []
+            winners: [],
+            isBarChart: true,
+            isVisible: false
         };
 
         this.fillData = this.fillData.bind(this);
+        this.handleClick = this.handleClick.bind(this);
     }
 
     fillData(data) {
@@ -21,41 +25,57 @@ export class Winners extends Component {
         });
     }
 
+    handleClick(){
+        var newChart = !this.state.isBarChart;
+
+        this.setState({
+                isBarChart: newChart
+            });
+    }
+
     render() {
         var data = this.state.winners.map(x => ({label: x.name, y: x.winCount}));
+        var options;
 
-        const options = {
-            data: [
-                {
-                    type: "column",
-                    dataPoints: data
-                }
-            ]
-        };
-
-        const options2 = {
-            data: [
-                {
-                    type: "pie",
-                    indexLabel: "{label} {y}",
-                    dataPoints: data
-                }
-            ]
-        };
+        if (this.state.isBarChart) {
+            options = {
+                data: [
+                    {
+                        type: "column",
+                        dataPoints: data
+                    }
+                ]
+            };
+        }
+        else {
+            options = {
+                data: [
+                    {
+                        type: "pie",
+                        indexLabel: "{label} {y}",
+                        dataPoints: data
+                    }
+                ]
+            };
+        }
 
         return (
             <div>
                 <h2>{this.props.pageTitle}</h2>
-                <p style={{color:"red", textAlign:"center"}}>*Papildomai pridėti pie chart</p>
                 <br />
                 <DataRangeForm api={this.props.api} callback={this.fillData} />
                 <br />
                 {
                     this.state.winners.length > 0 &&
+                    <div>
+                    <Button variant="primary" onClick={this.handleClick} disabled={this.state.isVisible}>
+                        {this.state.isBarChart ? "Rodyti skritulinę diagramą" : "Rodyti stulpelinę diagramą"}
+                    </Button>
+                    <br />
+                    <br />
                     <div style={{position:"relative", right:"6em"}}>
                         <CanvasJSChart options={options} />
-                        <br />
-                        <CanvasJSChart options={options2} />
+                    </div>
                     </div>
                 }
             </div>
