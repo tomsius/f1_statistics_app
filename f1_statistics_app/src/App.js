320b23321fcf2601d26c72f707193400f91c4501
Tomas Kaselynas <tomas.kaselynas@visma.com>
2021-02-21 13:29:50 +0200

    Add track winners component

--- f1_statistics_app/src/App.js
+++ f1_statistics_app/src/App.js
@@ -4,9 +4,10 @@ import './App.css';
 import { Navigation } from './components/Navigation';
 import { Home } from './components/Home';
 import { Wins } from './components/Statistics/Wins/Wins';
-import {Winners} from './components/Statistics/Wins/Winners';
+import { Winners } from './components/Statistics/Wins/Winners';
 import { WinnersPercent } from './components/Statistics/Wins/WinnersPercent';
-import {WinnersUnique} from './components/Statistics/Wins/WinnersUnique';
+import { WinnersUnique } from './components/Statistics/Wins/WinnersUnique';
+import { WinnersByTrack } from './components/Statistics/Wins/WinnersByTrack';
 import { NotFound } from './components/NotFound';
 
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
@@ -17,21 +18,21 @@ export default class App extends Component {
 
     return (
       <BrowserRouter>
-        <div className="container" style={{marginBottom:"50px"}}>
+        <div className="container" style={{ marginBottom: "50px" }}>
           <Navigation />
         </div>
         <Switch>
           <Route path='/' component={Home} exact />
 
           <Route path='/wins' component={Wins} exact />
-          
+
           <Route path='/wins/drivers' exact render={(props) => (<Winners {...props} api={"wins/drivers"} pageTitle={"Lenktynininkų laimėjimai"} />)} />
           <Route path='/wins/constructors' exact render={(props) => (<Winners {...props} api={"wins/constructors"} pageTitle={"Komandų laimėjimai"} />)} />
           <Route path='/wins/driverspercent' exact render={(props) => (<WinnersPercent {...props} api={"wins/drivers/percent"} pageTitle={"Lenktynininkų laimėjimų procentas"} />)} />
           <Route path='/wins/constructorspercent' exact render={(props) => (<WinnersPercent {...props} api={"wins/constructors/percent"} pageTitle={"Komandų laimėjimų procentas"} />)} />
           <Route path='/wins/driversunique' exact render={(props) => (<WinnersUnique {...props} api={"wins/drivers/uniqueperseason"} pageTitle={"Skirtingų lenktynininkų, laimėjusių lenktynes, skaičius"} />)} />
           <Route path='/wins/constructorsunique' exact render={(props) => (<WinnersUnique {...props} api={"wins/constructors/uniqueperseason"} pageTitle={"Skirtingų komandų, laimėjusių lenktynes, skaičius"} />)} />
-          <Route path='/wins/driversbytrack' component={Wins} exact />
+          <Route path='/wins/driversbytrack' exact render={(props) => (<WinnersByTrack {...props} api={"wins/drivers/tracks"} pageTitle={"Lenktynininkų laimėjimai pagal trasą"} />)} />
 
           <Route path="*" component={NotFound} />
         </Switch>
---
+++ f1_statistics_app/src/components/Statistics/Wins/WinnersByTrack.js
@@ -0,0 +1,92 @@
+import React, { Component } from 'react';
+import { DataRangeForm } from '../../DataRangeForm';
+import CanvasJSReact from '../../../canvasjs.react';
+import { Button, ButtonGroup, ToggleButton } from 'react-bootstrap';
+
+var CanvasJS = CanvasJSReact.CanvasJS;
+var CanvasJSChart = CanvasJSReact.CanvasJSChart;
+
+export class WinnersByTrack extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            winnersByTrack: [],
+            selectedTrack: ""
+        };
+
+        this.fillData = this.fillData.bind(this);
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    fillData(data) {
+        this.setState({
+            winnersByTrack: data
+        });
+    }
+
+    handleClick(event) {
+        event.preventDefault();
+
+        this.setState({
+            selectedTrack: event.currentTarget.value
+        });
+    }
+
+    render() {
+        var options;
+        if (this.state.winnersByTrack.length > 0 && this.state.selectedTrack !== "") {
+            var data = this.state.winnersByTrack.filter(x => x.name == this.state.selectedTrack).map(x => x.winners)[0].map(x => ({ label: x.name, y: x.winCount }));
+            options = {
+                title: {
+                    text: "„" + this.state.selectedTrack + "“ trasoje laimėję lenktynininkai"
+                },
+                axisY: {
+                    interval: 1,
+                    minimum: 0
+                },
+                data: [
+                    {
+                        type: "column",
+                        dataPoints: data
+                    }
+                ]
+            };
+        }
+
+        return (
+            <div>
+                <h2>{this.props.pageTitle}</h2>
+                <br />
+                <DataRangeForm api={this.props.api} callback={this.fillData} />
+                <br />
+                {
+                    this.state.winnersByTrack.length > 0 &&
+                    <div>
+                        <ButtonGroup toggle vertical>
+                            {this.state.winnersByTrack.map((track) => (
+                                <ToggleButton
+                                    key={track.name}
+                                    type="radio"
+                                    variant="secondary"
+                                    name="radio"
+                                    value={track.name}
+                                    checked={this.state.selectedTrack === track.name}
+                                    onChange={this.handleClick}
+                                >
+                                    {track.name}
+                                </ToggleButton>
+                            ))}
+                        </ButtonGroup>
+                        <br />
+                        <br />
+                        {this.state.selectedTrack !== "" &&
+                            <div style={{ position: "relative", right: "6em" }}>
+                                <CanvasJSChart options={options} />
+                            </div>
+                        }
+                    </div>
+                }
+            </div>
+        );
+    }
+}
