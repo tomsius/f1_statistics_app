4d93885349130871dbeb00c49e7d9a9e6951d6fe
Tomas Kaselynas <tomas.kaselynas@visma.com>
2021-02-21 18:00:00 +0200

    Add polesitters component

---
+++ f1_statistics_app/src/components/Statistics/Poles/PoleSitters.js
@@ -0,0 +1,84 @@
+import React, { Component } from 'react';
+import { DataRangeForm } from '../../DataRangeForm';
+import CanvasJSReact from '../../../canvasjs.react';
+import { Button } from 'react-bootstrap';
+
+var CanvasJS = CanvasJSReact.CanvasJS;
+var CanvasJSChart = CanvasJSReact.CanvasJSChart;
+
+export class PoleSitters extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            poleSitters: [],
+            isBarChart: true,
+            isVisible: false
+        };
+
+        this.fillData = this.fillData.bind(this);
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    fillData(data) {
+        this.setState({
+            poleSitters: data
+        });
+    }
+
+    handleClick() {
+        var newChart = !this.state.isBarChart;
+
+        this.setState({
+            isBarChart: newChart
+        });
+    }
+
+    render() {
+        var data = this.state.poleSitters.map(x => ({ label: x.name, y: x.poleCount }));
+        var options;
+
+        if (this.state.isBarChart) {
+            options = {
+                data: [
+                    {
+                        type: "column",
+                        dataPoints: data
+                    }
+                ]
+            };
+        }
+        else {
+            options = {
+                data: [
+                    {
+                        type: "pie",
+                        indexLabel: "{label} {y}",
+                        dataPoints: data
+                    }
+                ]
+            };
+        }
+
+        return (
+            <div>
+                <h2>{this.props.pageTitle}</h2>
+                <br />
+                <DataRangeForm api={this.props.api} callback={this.fillData} />
+                <br />
+                {
+                    this.state.poleSitters.length > 0 &&
+                    <div>
+                        <Button variant="primary" onClick={this.handleClick} disabled={this.state.isVisible}>
+                            {this.state.isBarChart ? "Rodyti skritulinę diagramą" : "Rodyti stulpelinę diagramą"}
+                        </Button>
+                        <br />
+                        <br />
+                        <div style={{ position: "relative", right: "6em" }}>
+                            <CanvasJSChart options={options} />
+                        </div>
+                    </div>
+                }
+            </div>
+        );
+    }
+}
