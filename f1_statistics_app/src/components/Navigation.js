d5edd2a1b0556cfbc76b34f0d0ed3e0d0aaf1728
Tomas Kašelynas <tomaskas98@gmail.com>
2021-03-26 10:35:54 +0200

    Refactoring

--- f1_statistics_app/src/components/Navigation.js
+++ f1_statistics_app/src/components/Navigation.js
@@ -9,22 +9,24 @@ export class Navigation extends Component {
 
     render() {
         return (
-            <Navbar bg="dark" variant="dark" expand="lg">
-                <Navbar.Brand href="/">Pradžia</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="mr-auto">
-                        <NavLink className="d-inline p-2 text-light" to="/wins">Laimėjimai</NavLink>
-                        <NavLink className="d-inline p-2 text-light" to="/poles">„Pole“ pozicijos</NavLink>
-                        <NavLink className="d-inline p-2 text-light" to="/fastestlaps">Greičiausi ratai</NavLink>
-                        <NavLink className="d-inline p-2 text-light" to="/points">Taškai</NavLink>
-                        <NavLink className="d-inline p-2 text-light" to="/podiums">Podiumai</NavLink>
-                        <NavLink className="d-inline p-2 text-light" to="/leadings">Pirmavimai</NavLink>
-                        <NavLink className="d-inline p-2 text-light" to="/nationalities">Pilietybės</NavLink>
-                        <NavLink className="d-inline p-2 text-light" to="/misc">Įvairios</NavLink>
-                    </Nav>
-                </Navbar.Collapse>
-        </Navbar>
+            <div className="container" style={{ marginBottom: "50px" }}>
+                <Navbar bg="dark" variant="dark" expand="lg">
+                    <NavLink className="d-inline p-2 text-light" to="/">Pradžia</NavLink>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="mr-auto">
+                            <NavLink className="d-inline p-2 text-light" to="/wins">Laimėjimai</NavLink>
+                            <NavLink className="d-inline p-2 text-light" to="/poles">„Pole“ pozicijos</NavLink>
+                            <NavLink className="d-inline p-2 text-light" to="/fastestlaps">Greičiausi ratai</NavLink>
+                            <NavLink className="d-inline p-2 text-light" to="/points">Taškai</NavLink>
+                            <NavLink className="d-inline p-2 text-light" to="/podiums">Podiumai</NavLink>
+                            <NavLink className="d-inline p-2 text-light" to="/leadings">Pirmavimai</NavLink>
+                            <NavLink className="d-inline p-2 text-light" to="/nationalities">Pilietybės</NavLink>
+                            <NavLink className="d-inline p-2 text-light" to="/misc">Įvairios</NavLink>
+                        </Nav>
+                    </Navbar.Collapse>
+            </Navbar>
+        </div>
         );
     }
 }
