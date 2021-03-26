23bef051d69d92f767725feb570bd1a702a4f32c
Tomas Kašelynas <tomaskas98@gmail.com>
2021-03-26 11:43:10 +0200

    Fix footer

--- f1_statistics_app/src/components/Footer.js
+++ f1_statistics_app/src/components/Footer.js
@@ -7,15 +7,15 @@ export class Footer extends Component {
 
     render() {
         return (
-            <div class="d-flex flex-column" style={{marginTop: "100px"}}>
-                <footer class="footer">
+            <div className="d-flex flex-column" style={{marginTop: "100px"}}>
+                <footer className="footer">
                     <hr />
                     <div>
-                        <span>Tomas Kašelynas <a href="https://github.com/tomsius" target="_blank"><i class="fa" style={{fontSize: "24px"}}>&#xf09b;</i></a></span>
+                        <span>Tomas Kašelynas <a href="https://github.com/tomsius" target="_blank"><i className="fa" style={{fontSize: "24px"}}>&#xf09b;</i></a></span>
                         <br />
                         <span>Lenktynių rezultatų portalas {new Date().getFullYear()}</span>
                     </div>
-                    <div class="ml-auto">
+                    <div className="ml-auto">
                         <a href="https://github.com/tomsius/f1_statistics" target="_blank">Back-end</a> | <a href="https://github.com/tomsius/f1_statistics_app" target="_blank">Front-end</a>
                     </div>
                 </footer>
