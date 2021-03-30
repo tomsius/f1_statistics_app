a683f5aedeb714c77744454512c0be21526b5591
Tomas Kašelynas <tomaskas98@gmail.com>
2021-03-30 09:36:34 +0300

    Refactoring

--- f1_statistics_app/src/components/Statistics/Poles/Poles.js
+++ f1_statistics_app/src/components/Statistics/Poles/Poles.js
@@ -12,9 +12,9 @@ export class Poles extends Component {
                 <a href="/poles/drivers" style={{marginBottom:"15px"}}>
                 <Card style={{display:"flex", height:"100%"}}>
                     <Card.Body>
-                        <Card.Title style={{fontWeight:"bold"}}>Lenktynininkų pole pozicijos</Card.Title>
+                        <Card.Title style={{fontWeight:"bold"}}>Lenktynininkų „pole“ pozicijos</Card.Title>
                         <Card.Text>
-                            Atvaizduojami lenktynininkai, kurie yra laimėję pole poziciją (kvalifikacijoje buvo greičiausias) bent vieną kartą, ir jų pole pozicijų skaičius.
+                            Atvaizduojami lenktynininkai, kurie yra laimėję „pole“ poziciją (kvalifikacijoje buvo greičiausias) bent vieną kartą, ir jų „pole“ pozicijų skaičius.
                         </Card.Text>
                     </Card.Body>
                 </Card>
@@ -22,9 +22,9 @@ export class Poles extends Component {
                 <a href="/poles/constructors" style={{marginBottom:"15px"}}>
                 <Card style={{display:"flex", height:"100%"}}>
                     <Card.Body>
-                        <Card.Title style={{fontWeight:"bold"}}>Komandų pole pozicijos</Card.Title>
+                        <Card.Title style={{fontWeight:"bold"}}>Komandų „pole“ pozicijos</Card.Title>
                         <Card.Text>
-                            Atvaizduojamos komandos, kurios yra laimėję pole poziciją (kvalifikacijoje komandos lenktynininkas buvo greičiausias) bent vieną kartą, ir jų pole pozicijų skaičius.
+                            Atvaizduojamos komandos, kurios yra laimėję „pole“ poziciją (kvalifikacijoje komandos lenktynininkas buvo greičiausias) bent vieną kartą, ir jų „pole“ pozicijų skaičius.
                         </Card.Text>
                     </Card.Body>
                 </Card>
@@ -32,9 +32,9 @@ export class Poles extends Component {
                 <a href="/poles/driversunique" style={{marginBottom:"15px"}}>
                 <Card style={{display:"flex", height:"100%"}}>
                     <Card.Body>
-                        <Card.Title style={{fontWeight:"bold"}}>Skirtingų lenktynininkų, laimėjusių pole poziciją, skaičius</Card.Title>
+                        <Card.Title style={{fontWeight:"bold"}}>Skirtingų lenktynininkų, laimėjusių „pole“ poziciją, skaičius</Card.Title>
                         <Card.Text>
-                            Atvaizduojama, kiek skirtingų lenktynininkų yra laimėję pole poziciją (kvalifikacijoje buvo greičiausi) tam tikrą sezoną. Užvedus ant grafiko, galima pamatyti lenktynininkus, kurie laimėjo pole poziciją.
+                            Atvaizduojama, kiek skirtingų lenktynininkų yra laimėję „pole“ poziciją (kvalifikacijoje buvo greičiausi) tam tikrą sezoną. Užvedus ant grafiko, galima pamatyti lenktynininkus, kurie laimėjo „pole“ poziciją.
                         </Card.Text>
                     </Card.Body>
                 </Card>
@@ -42,9 +42,9 @@ export class Poles extends Component {
                 <a href="/poles/constructorsunique" style={{marginBottom:"15px"}}>
                 <Card style={{display:"flex", height:"100%"}}>
                     <Card.Body>
-                        <Card.Title style={{fontWeight:"bold"}}>Skirtingų komandų, laimėjusių pole poziciją, skaičius</Card.Title>
+                        <Card.Title style={{fontWeight:"bold"}}>Skirtingų komandų, laimėjusių „pole“ poziciją, skaičius</Card.Title>
                         <Card.Text>
-                            Atvaizduojama, kiek skirtingų komandų yra laimėjusios pole poziciją (kvalifikacijoje komandos lenktynininkas buvo greičiausias) tam tikrą sezoną. Užvedus ant grafiko, galima pamatyti komandas, kurios laimėjo pole poziciją.
+                            Atvaizduojama, kiek skirtingų komandų yra laimėjusios „pole“ poziciją (kvalifikacijoje komandos lenktynininkas buvo greičiausias) tam tikrą sezoną. Užvedus ant grafiko, galima pamatyti komandas, kurios laimėjo „pole“ poziciją.
                         </Card.Text>
                     </Card.Body>
                 </Card>
