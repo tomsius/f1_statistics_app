3a510e67dbd7fd079390dbf440bfe1074c3d0b54
Tomas Kašelynas <tomaskas98@gmail.com>
2021-03-31 12:25:01 +0300

    Add secondary axis

--- f1_statistics_app/src/components/Statistics/Points/WinnerPoints.js
+++ f1_statistics_app/src/components/Statistics/Points/WinnerPoints.js
@@ -30,7 +30,11 @@ export class WinnerPoints extends Component {
             axisYGridThickness: 1,
             axisYMinimum: 0,
             axisYMaximum: '',
-            axisYInterval: 50
+            axisYInterval: 50,
+
+            axisY2Title: "Lenktynių skaičius, vnt.",
+            axisY2LabelAngle: 0,
+            axisY2Interval: 2
         };
 
         this.fillData = this.fillData.bind(this);
@@ -76,7 +80,11 @@ export class WinnerPoints extends Component {
             axisYGridThickness: 1,
             axisYMinimum: 0,
             axisYMaximum: '',
-            axisYInterval: 50
+            axisYInterval: 50,
+
+            axisY2Title: "Lenktynių skaičius, vnt.",
+            axisY2LabelAngle: 0,
+            axisY2Interval: 2
         }, () => {
             callback();
         });
@@ -156,9 +164,72 @@ export class WinnerPoints extends Component {
                     gridThickness: this.state.axisYGridThickness
                 },
                 toolTip:{   
-                    content: "Laimėtojas: {winner} ({y} taškų)" 
+                    contentFormatter: "",
+                    content: "Laimėtojas {label} metais: {winner} ({y} taškų)" 
                 }
             };
+
+            if (this.state.type === "line") {
+                var racesData = this.state.winnerPoints.map(x => ({ label: x.season, x: x.season, y: x.racesCount }));
+
+                if (this.state.axisY2Maximum === '') {
+                    var defaultMaximum2 = -1;
+                    for (let i = 0; i < this.state.winnerPoints.length; i++) {
+                        if (defaultMaximum2 < this.state.winnerPoints[i].racesCount) {
+                            defaultMaximum2 = this.state.winnerPoints[i].racesCount;
+                        }
+                    }
+    
+                    defaultMaximum2 = defaultMaximum2 % this.state.axisYInterval === 0 ? defaultMaximum2 : (defaultMaximum2 + (this.state.axisYInterval - (defaultMaximum2 % this.state.axisYInterval)));
+                }
+
+                options["axisY"]["titleFontColor"] = "#4F81BC";
+                options["axisY"]["lineColor"] = "#4F81BC";
+                options["axisY"]["labelFontColor"] = "#4F81BC";
+                options["axisY"]["tickColor"] = "#4F81BC";
+                options["axisY2"] = {};
+                options["axisY2"]["title"] = this.state.axisY2Title;
+                options["axisY2"]["minimum"] = 0;
+                options["axisY2"]["interval"] = this.state.axisY2Interval;
+                options["axisY2"]["labelAngle"] = this.state.axisY2LabelAngle;
+                options["axisY2"]["titleFontColor"] = "#C0504E";
+                options["axisY2"]["lineColor"] = "#C0504E";
+                options["axisY2"]["labelFontColor"] = "#C0504E";
+                options["axisY2"]["tickColor"] = "#C0504E";
+                options["data"][0]["showInLegend"] = true;
+                options["data"][0]["name"] = "Taškų skaičius";
+                options["data"].push({ type: "line", showInLegend: true, name: "Lenktynių skaičius", axisYType: "secondary", dataPoints: racesData });
+                options["toolTip"]["shared"] = true;
+                options["toolTip"]["contentFormatter"] = function (e) {
+                        var content = "";
+                        content += "Laimėtojas " + e.entries[0].dataPoint.label + " metais: " + e.entries[0].dataPoint.winner + " (" + e.entries[0].dataPoint.y + " taškų)<br />";
+                        content += "Lenktynių skaičius " + e.entries[1].dataPoint.label + " metais: " + e.entries[1].dataPoint.y;
+                        return content;
+                    }
+                options["legend"] = {};
+                options["legend"]["cursor"] = "pointer";
+                options["legend"]["itemclick"] = function (e) {
+                    if (e.dataSeries.name === "Taškų skaičius") {
+                        return;
+                    }
+                    if (typeof (e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
+                        e.chart.options["axisY2"] = {};
+                        e.dataSeries.visible = false;
+                    }
+                    else {
+                        e.chart.options["axisY2"]["title"] = "Lenktynių skaičius, vnt.";
+                        e.chart.options["axisY2"]["minimum"] = 0;
+                        e.chart.options["axisY2"]["interval"] = 2;
+                        e.chart.options["axisY2"]["labelAngle"] = 0;
+                        e.chart.options["axisY2"]["titleFontColor"] = "#C0504E";
+                        e.chart.options["axisY2"]["lineColor"] = "#C0504E";
+                        e.chart.options["axisY2"]["labelFontColor"] = "#C0504E";
+                        e.chart.options["axisY2"]["tickColor"] = "#C0504E";
+                        e.dataSeries.visible = true;
+                    }
+                    e.chart.render();
+                }
+            }
         }
 
         return (
@@ -196,6 +267,10 @@ export class WinnerPoints extends Component {
                                     axisyminimum={this.state.axisYMinimum}
                                     axisymaximum={this.state.axisYMaximum !== '' ? this.state.axisYMaximum : defaultMaximum}
                                     axisyinterval={this.state.axisYInterval}
+                                    secondaxis={this.state.type === "line" ? 1 : undefined}
+                                    axisy2title={this.state.axisY2Title}
+                                    axisy2labelangle={this.state.axisY2LabelAngle}
+                                    axisy2interval={this.state.axisY2Interval}
                                 />
                         <br />
                         <br />
