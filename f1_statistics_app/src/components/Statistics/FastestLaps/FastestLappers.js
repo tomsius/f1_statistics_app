e0d83ac08f087d66595aabd5103c5809d81b1e85
Tomas Kašelynas <tomaskas98@gmail.com>
2021-04-01 12:00:03 +0300

    Add stacked column chart

--- f1_statistics_app/src/components/Statistics/FastestLaps/FastestLappers.js
+++ f1_statistics_app/src/components/Statistics/FastestLaps/FastestLappers.js
@@ -23,6 +23,7 @@ export class FastestLappers extends Component {
             type: "column",
 
             axisXTitle: this.props.axisName,
+            axisXTitle2: "Metai",
             axisXLabelAngle: 0,
             axisXGridThickness: 0,
 
@@ -60,7 +61,7 @@ export class FastestLappers extends Component {
     handleOptionsChange(event) {
         const { name, value, checked, type } = event.target;
         var valueToUpdate = type === 'checkbox' ? checked : value;
-        
+
         if (name === 'axisYInterval') {
             valueToUpdate = parseInt(value);
         }
@@ -80,6 +81,7 @@ export class FastestLappers extends Component {
             type: "column",
 
             axisXTitle: this.props.axisName,
+            axisXTitle2: "Metai",
             axisXLabelAngle: 0,
             axisXGridThickness: 0,
 
@@ -115,18 +117,25 @@ export class FastestLappers extends Component {
 
     render() {
         if (this.state.fastestLappers.length > 0) {
-            var totalFastest = this.calculateTotalFastest(this.state.fastestLappers);
-            var data = this.state.fastestLappers.map((x, index) => ({ label: x.name, x: index + 1, y: x.fastestLapsCount, percentage: Math.round((x.fastestLapsCount / totalFastest * 100) * 100) / 100 }));
+            if (this.state.type !== "stackedColumn") {
+                var totalFastest = this.calculateTotalFastest(this.state.fastestLappers);
+                var data = this.state.fastestLappers.map((x, index) => ({ label: x.name, x: index + 1, y: x.fastestLapsCount, percentage: Math.round((x.fastestLapsCount / totalFastest * 100) * 100) / 100 }));
 
-            if (this.state.axisYMaximum === '') {
-                var defaultMaximum = -1;
-                for (let i = 0; i < this.state.fastestLappers.length; i++) {
-                    if (defaultMaximum < this.state.fastestLappers[i].fastestLapsCount) {
-                        defaultMaximum = this.state.fastestLappers[i].fastestLapsCount;
+                if (this.state.axisYMaximum === '') {
+                    var defaultMaximum = -1;
+                    for (let i = 0; i < this.state.fastestLappers.length; i++) {
+                        if (defaultMaximum < this.state.fastestLappers[i].fastestLapsCount) {
+                            defaultMaximum = this.state.fastestLappers[i].fastestLapsCount;
+                        }
                     }
+
+                    defaultMaximum = defaultMaximum % this.state.axisYInterval === 0 ? defaultMaximum : (defaultMaximum + (this.state.axisYInterval - (defaultMaximum % this.state.axisYInterval)));
                 }
-    
-                defaultMaximum = defaultMaximum % this.state.axisYInterval === 0 ? defaultMaximum : (defaultMaximum + (this.state.axisYInterval - (defaultMaximum % this.state.axisYInterval)));
+            }
+            else {
+                var data = this.state.fastestLappers.map(x => ({ type: "stackedColumn", showInLegend: true, name: x.name, dataPoints: x.fastestLapsByYear.map(yearFastestLap => ({ label: yearFastestLap.year, x: yearFastestLap.year, y: yearFastestLap.fastestLapCount })) }));
+
+                defaultMaximum = 25;
             }
 
             var options = {
@@ -139,15 +148,15 @@ export class FastestLappers extends Component {
                 title: {
                     text: this.state.title
                 },
-                data: [
+                data: this.state.type !== "stackedColumn" ? [
                     {
                         type: this.state.type,
                         dataPoints: data,
                         indexLabel: this.state.type === 'column' ? "" : "{label} {y}"
                     }
-                ],
+                ] : data,
                 axisX: {
-                    title: this.state.axisXTitle,
+                    title: this.state.type !== "stackedColumn" ? this.state.axisXTitle : this.state.axisXTitle2,
                     labelAngle: this.state.axisXLabelAngle,
                     interval: 1,
                     gridThickness: this.state.axisXGridThickness,
@@ -163,8 +172,20 @@ export class FastestLappers extends Component {
                     labelAngle: this.state.axisYLabelAngle,
                     gridThickness: this.state.axisYGridThickness
                 },
-                toolTip:{   
+                toolTip: this.state.type !== "stackedColumn" ? {
                     content: this.state.type === 'column' ? "{label}: {y}" : "{label}: {percentage}%"
+                } : {
+                    shared: true,
+                    content: function (e) {
+                        var content = e.entries[0].dataPoint.x + "<br />";
+                        var total = 0;
+                        for (let i = 0; i < e.entries.length; i++) {
+                            content += e.entries[i].dataSeries.name + ": " + e.entries[i].dataPoint.y + "<br />";
+                            total += e.entries[i].dataPoint.y;
+                        }
+                        content += "Iš viso: " + total;
+                        return content;
+                    }
                 }
             };
         }
@@ -173,34 +194,34 @@ export class FastestLappers extends Component {
             <div>
                 <h1>{this.props.pageTitle}</h1>
                 <br />
-                <p style={{color:"red", textAlign:"center"}}>*Greičiausių ratų duomenys prieinami tik nuo 2004 metų.</p>
+                <p style={{ color: "red", textAlign: "center" }}>*Greičiausių ratų duomenys prieinami tik nuo 2004 metų.</p>
                 <br />
                 <DataRangeForm api={this.props.api} callback={this.fillData} />
                 <br />
                 {
                     this.state.fastestLappers.length > 0 &&
                     <div>
-                        <Button variant="primary" onClick={() => this.setState({modalShow: true})}>
+                        <Button variant="primary" onClick={() => this.setState({ modalShow: true })}>
                             Keisti grafiko parinktis
                         </Button>
-                        <ChartOptionsModal 
+                        <ChartOptionsModal
                             animation={false}
                             size="lg"
-                            show={this.state.modalShow} 
-                            onHide={() => this.setState({modalShow: false})} 
-                            handleoptionschange={this.handleOptionsChange} 
+                            show={this.state.modalShow}
+                            onHide={() => this.setState({ modalShow: false })}
+                            handleoptionschange={this.handleOptionsChange}
                             setdefaultvalues={this.setDefaultValues}
                             title={this.state.title}
                             exportfilename={this.state.exportFileName}
                             interactivityenabled={this.state.interactivityEnabled ? 1 : 0}
-                            themes={[{value: "light1", content: "Light1"}, {value: "light2", content: "Light2"}, {value: "dark1", content: "Dark1"}, {value: "dark2", content: "Dark2"}]}
+                            themes={[{ value: "light1", content: "Light1" }, { value: "light2", content: "Light2" }, { value: "dark1", content: "Dark1" }, { value: "dark2", content: "Dark2" }]}
                             currenttheme={this.state.theme}
-                            types={[{type: "column", name: "Stulpelinė"}, {type: "pie", name: "Skritulinė"}]}
+                            types={[{ type: "column", name: "Stulpelinė" }, {type: "stackedColumn", name: "Stulpelinė (sluoksniuota)"}, { type: "pie", name: "Skritulinė" }]}
                             currenttype={this.state.type}
                             axisxtitle={this.state.axisXTitle}
                             axisxlabelangle={this.state.axisXLabelAngle}
                             axisxgridthickness={this.state.axisXGridThickness}
-                            axisytitle={this.state.axisYTitle}
+                            axisxtitle={this.state.type !== "stackedColumn" ? this.state.axisXTitle : this.state.axisXTitle2}
                             axisylabelangle={this.state.axisYLabelAngle}
                             axisygridthickness={this.state.axisYGridThickness}
                             axisyminimum={this.state.axisYMinimum}
