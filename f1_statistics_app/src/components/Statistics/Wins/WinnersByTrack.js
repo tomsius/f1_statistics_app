5fd86e9f856bb30f049c92f08b825ba4357e8377
Tomas Kašelynas <tomaskas98@gmail.com>
2021-04-07 10:01:30 +0300

    Refactoring

--- f1_statistics_app/src/components/Statistics/Wins/WinnersByTrack.js
+++ f1_statistics_app/src/components/Statistics/Wins/WinnersByTrack.js
@@ -34,7 +34,7 @@ export class WinnersByTrack extends Component {
             axisYMinimum: 0,
             axisYMaximum: '',
             axisYInterval: 1,
-            
+
             titleFont: "Calibri",
             axisXFont: "Calibri",
             axisYFont: "Calibri"
@@ -69,7 +69,7 @@ export class WinnersByTrack extends Component {
     handleOptionsChange(event) {
         const { name, value, checked, type } = event.target;
         var valueToUpdate = type === 'checkbox' ? checked : value;
-        
+
         if (name === 'axisYInterval') {
             valueToUpdate = parseInt(value);
         }
@@ -98,7 +98,7 @@ export class WinnersByTrack extends Component {
             axisYMinimum: 0,
             axisYMaximum: '',
             axisYInterval: 1,
-            
+
             titleFont: "Calibri",
             axisXFont: "Calibri",
             axisYFont: "Calibri"
@@ -129,8 +129,9 @@ export class WinnersByTrack extends Component {
 
     render() {
         if (this.state.winnersByTrack.length > 0 && this.state.selectedTrack !== "") {
-            var data = this.state.winnersByTrack.filter(x => x.name == this.state.selectedTrack).map(x => x.winners)[0].map((x, index) => ({ label: x.name, x: index + 1, y: x.winCount }));
-            var participationsData = this.state.winnersByTrack.filter(x => x.name == this.state.selectedTrack).map(x => x.winners)[0].map((x, index) => ({ label: x.name, x: index + 1, y: x.participationsCount }));
+            var selectedTrack = this.state.winnersByTrack.filter(x => x.name == this.state.selectedTrack);
+            var data = selectedTrack.map(x => x.winners)[0].map((x, index) => ({ label: x.name, x: index + 1, y: x.winCount }));
+            var participationsData = selectedTrack.map(x => x.winners)[0].map((x, index) => ({ label: x.name, x: index + 1, y: x.participationsCount }));
 
             if (this.state.axisYMaximum === '') {
                 var defaultMaximum = -1;
@@ -139,7 +140,7 @@ export class WinnersByTrack extends Component {
                         defaultMaximum = participationsData[i].y;
                     }
                 }
-    
+
                 defaultMaximum = defaultMaximum % this.state.axisYInterval === 0 ? defaultMaximum : (defaultMaximum + (this.state.axisYInterval - (defaultMaximum % this.state.axisYInterval)));
             }
 
@@ -188,12 +189,12 @@ export class WinnersByTrack extends Component {
                     labelFontFamily: this.state.axisYFont
                 },
                 legend: {
-                    cursor:"pointer",
-		            itemclick: function (e) {
+                    cursor: "pointer",
+                    itemclick: function (e) {
                         if (e.dataSeries.name === "Laimėjimai") {
                             return;
                         }
-                        if (typeof(e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
+                        if (typeof (e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
                             e.dataSeries.visible = false;
                         }
                         else {
@@ -235,44 +236,44 @@ export class WinnersByTrack extends Component {
                         <br />
                         <br />
                         {this.state.selectedTrack !== "" &&
-                        <div>
-                            <Button variant="primary" onClick={() => this.setState({modalShow: true})}>
-                                Keisti grafiko parinktis
+                            <div>
+                                <Button variant="primary" onClick={() => this.setState({ modalShow: true })}>
+                                    Keisti grafiko parinktis
                             </Button>
-                            <ChartOptionsModal 
-                                animation={false}
-                                size="lg"
-                                show={this.state.modalShow} 
-                                onHide={() => this.setState({modalShow: false})} 
-                                handleoptionschange={this.handleOptionsChange} 
-                                setdefaultvalues={this.setDefaultValues}
-                                title={this.state.title}
-                                exportfilename={this.state.exportFileName}
-                                interactivityenabled={this.state.interactivityEnabled ? 1 : 0}
-                                themes={[{value: "light1", content: "Light1"}, {value: "light2", content: "Light2"}, {value: "dark1", content: "Dark1"}, {value: "dark2", content: "Dark2"}]}
-                                currenttheme={this.state.theme}
-                                types={[{type: "column", name: "Stulpelinė"}]}
-                                currenttype={this.state.type}
-                                axisxtitle={this.state.axisXTitle}
-                                axisxlabelangle={this.state.axisXLabelAngle}
-                                axisxgridthickness={this.state.axisXGridThickness}
-                                axisytitle={this.state.axisYTitle}
-                                axisylabelangle={this.state.axisYLabelAngle}
-                                axisygridthickness={this.state.axisYGridThickness}
-                                axisyminimum={this.state.axisYMinimum}
-                                axisymaximum={this.state.axisYMaximum !== '' ? this.state.axisYMaximum : defaultMaximum}
-                                axisyinterval={this.state.axisYInterval}
-                                fonts={["Calibri", "Optima", "Candara", "Verdana", "Geneva"]}
-                                currenttitlefont={this.state.titleFont}
-                                currentaxisxfont={this.state.axisXFont}
-                                currentaxisyfont={this.state.axisYFont}
-                            />
-                            <br />
-                            <br />
-                            <div style={{ position: "relative", right: "6em" }}>
-                                <CanvasJSChart options={options} />
-                            </div>
-                        </div>}
+                                <ChartOptionsModal
+                                    animation={false}
+                                    size="lg"
+                                    show={this.state.modalShow}
+                                    onHide={() => this.setState({ modalShow: false })}
+                                    handleoptionschange={this.handleOptionsChange}
+                                    setdefaultvalues={this.setDefaultValues}
+                                    title={this.state.title}
+                                    exportfilename={this.state.exportFileName}
+                                    interactivityenabled={this.state.interactivityEnabled ? 1 : 0}
+                                    themes={[{ value: "light1", content: "Light1" }, { value: "light2", content: "Light2" }, { value: "dark1", content: "Dark1" }, { value: "dark2", content: "Dark2" }]}
+                                    currenttheme={this.state.theme}
+                                    types={[{ type: "column", name: "Stulpelinė" }]}
+                                    currenttype={this.state.type}
+                                    axisxtitle={this.state.axisXTitle}
+                                    axisxlabelangle={this.state.axisXLabelAngle}
+                                    axisxgridthickness={this.state.axisXGridThickness}
+                                    axisytitle={this.state.axisYTitle}
+                                    axisylabelangle={this.state.axisYLabelAngle}
+                                    axisygridthickness={this.state.axisYGridThickness}
+                                    axisyminimum={this.state.axisYMinimum}
+                                    axisymaximum={this.state.axisYMaximum !== '' ? this.state.axisYMaximum : defaultMaximum}
+                                    axisyinterval={this.state.axisYInterval}
+                                    fonts={["Calibri", "Optima", "Candara", "Verdana", "Geneva"]}
+                                    currenttitlefont={this.state.titleFont}
+                                    currentaxisxfont={this.state.axisXFont}
+                                    currentaxisyfont={this.state.axisYFont}
+                                />
+                                <br />
+                                <br />
+                                <div style={{ position: "relative", right: "6em" }}>
+                                    <CanvasJSChart options={options} />
+                                </div>
+                            </div>}
                     </div>
                 }
             </div>
