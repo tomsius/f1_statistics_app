d6effc501812506f01fce1095f897c5b4f8379cf
Tomas Kašelynas <tomaskas98@gmail.com>
2021-04-08 12:06:51 +0300

    Add data options

--- f1_statistics_app/src/components/Statistics/Misc/GrandSlams.js
+++ f1_statistics_app/src/components/Statistics/Misc/GrandSlams.js
@@ -3,6 +3,7 @@ import { DataRangeForm } from '../../DataRangeForm';
 import CanvasJSReact from '../../../canvasjs.react';
 import { Button } from 'react-bootstrap';
 import { ChartOptionsModal } from '../../ChartOptionsModal';
+import { DataOptionsModal } from '../../DataOptionsModal';
 import { addWatermark, changeExportButtonsLanguage } from '../../../js/utils';
 
 var CanvasJS = CanvasJSReact.CanvasJS;
@@ -32,23 +33,31 @@ export class GrandSlams extends Component {
             axisYMinimum: 0,
             axisYMaximum: '',
             axisYInterval: 5,
-            
+
             titleFont: "Calibri",
             axisXFont: "Calibri",
-            axisYFont: "Calibri"
+            axisYFont: "Calibri",
+
+            from: 0,
+            to: '',
+            selectedNationalities: []
         };
 
         this.fillData = this.fillData.bind(this);
         this.calculateTotalGrandslams = this.calculateTotalGrandslams.bind(this);
         this.handleOptionsChange = this.handleOptionsChange.bind(this);
         this.setDefaultValues = this.setDefaultValues.bind(this);
+        this.setDefaultDataFilters = this.setDefaultDataFilters.bind(this);
+        this.filterData = this.filterData.bind(this);
+        this.getNationalities = this.getNationalities.bind(this);
+        this.initializeNationalities = this.initializeNationalities.bind(this);
         this.updateWindowSize = this.updateWindowSize.bind(this);
     }
 
     fillData(data) {
         this.setState({
             grandSlams: data
-        });
+        }, () => { this.initializeNationalities(this.getNationalities(this.state.grandSlams)) });
     }
 
     calculateTotalGrandslams(grandSlams) {
@@ -64,9 +73,14 @@ export class GrandSlams extends Component {
     handleOptionsChange(event) {
         const { name, value, checked, type } = event.target;
         var valueToUpdate = type === 'checkbox' ? checked : value;
-        
+
         if (name === 'axisYInterval') {
             valueToUpdate = parseInt(value);
+        }
+
+        if (name === 'selectedNationalities') {
+            valueToUpdate = this.state.selectedNationalities;
+            valueToUpdate.filter(x => x.nationality === value)[0].checked = checked;
         }
 
         this.setState({
@@ -93,12 +107,71 @@ export class GrandSlams extends Component {
             axisYMinimum: 0,
             axisYMaximum: '',
             axisYInterval: 5,
-            
+
             titleFont: "Calibri",
             axisXFont: "Calibri",
             axisYFont: "Calibri"
         }, () => {
             callback();
+        });
+    }
+
+    setDefaultDataFilters(callback) {
+        this.setState({
+            from: 0,
+            to: '',
+            selectedNationalities: []
+        }, () => {
+            this.initializeNationalities(this.getNationalities(this.state.grandSlams));
+            callback();
+        });
+    }
+
+    filterData(data) {
+        var filteredData = JSON.parse(JSON.stringify(data));
+
+        this.state.selectedNationalities.forEach(selectedNationality => {
+            if (selectedNationality.checked === false) {
+                for (let i = 0; i < filteredData.length; i++) {
+                    if (filteredData[i].nationality === selectedNationality.nationality) {
+                        filteredData.splice(i, 1);
+                        i--;
+                    }
+                }
+            }
+        });
+
+        filteredData = filteredData.filter(grandSlam => grandSlam.grandSlamCount >= this.state.from);
+
+        if (this.state.to !== '') {
+            filteredData = filteredData.filter(grandSlam => grandSlam.grandSlamCount <= this.state.to);
+        }
+
+        return filteredData;
+    }
+
+    getNationalities(data) {
+        var uniqueNationalities = new Set();
+
+        data.forEach(grandSlam => {
+            uniqueNationalities.add(grandSlam.nationality);
+        });
+
+        return [...uniqueNationalities];
+    }
+
+    initializeNationalities(nationalities) {
+        var nationalityObjects = [];
+        nationalities.forEach(nationality => {
+            var nationalityObject = { nationality: nationality, checked: true };
+
+            nationalityObjects.push(nationalityObject);
+        });
+
+        nationalityObjects.sort((a, b) => (a.nationality > b.nationality ? 1 : -1));
+
+        this.setState({
+            selectedNationalities: nationalityObjects
         });
     }
 
@@ -124,17 +197,18 @@ export class GrandSlams extends Component {
 
     render() {
         if (this.state.grandSlams !== null) {
-            var totalGrandSlams = this.calculateTotalGrandslams(this.state.grandSlams);
-            var data = this.state.grandSlams.map((x, index) => ({ label: x.name, x: index + 1, y: x.grandSlamCount, percentage: Math.round((x.grandSlamCount / totalGrandSlams * 100) * 100) / 100 }));
-            
+            var filteredData = this.filterData(this.state.grandSlams);
+            var totalGrandSlams = this.calculateTotalGrandslams(filteredData);
+            var data = filteredData.map((x, index) => ({ label: x.name, x: index + 1, y: x.grandSlamCount, percentage: Math.round((x.grandSlamCount / totalGrandSlams * 100) * 100) / 100 }));
+
             if (this.state.axisYMaximum === '') {
                 var defaultMaximum = -1;
-                for (let i = 0; i < this.state.grandSlams.length; i++) {
-                    if (defaultMaximum < this.state.grandSlams[i].grandSlamCount) {
-                        defaultMaximum = this.state.grandSlams[i].grandSlamCount;
+                for (let i = 0; i < filteredData.length; i++) {
+                    if (defaultMaximum < filteredData[i].grandSlamCount) {
+                        defaultMaximum = filteredData[i].grandSlamCount;
                     }
                 }
-    
+
                 defaultMaximum = defaultMaximum % this.state.axisYInterval === 0 ? defaultMaximum : (defaultMaximum + (this.state.axisYInterval - (defaultMaximum % this.state.axisYInterval)));
             }
 
@@ -177,7 +251,7 @@ export class GrandSlams extends Component {
                     titleFontFamily: this.state.axisYFont,
                     labelFontFamily: this.state.axisYFont
                 },
-                toolTip:{   
+                toolTip: {
                     content: this.state.type === 'column' ? "{label}: {y}" : "{label}: {percentage}%"
                 }
             };
@@ -192,22 +266,22 @@ export class GrandSlams extends Component {
                 {
                     this.state.grandSlams !== null &&
                     <div>
-                        <Button variant="primary" onClick={() => this.setState({modalShow: true})}>
+                        <Button variant="primary" onClick={() => this.setState({ modalShow: true })}>
                             Keisti grafiko parinktis
                         </Button>
-                        <ChartOptionsModal 
+                        <ChartOptionsModal
                             animation={false}
                             size="lg"
-                            show={this.state.modalShow} 
-                            onHide={() => this.setState({modalShow: false})} 
-                            handleoptionschange={this.handleOptionsChange} 
+                            show={this.state.modalShow}
+                            onHide={() => this.setState({ modalShow: false })}
+                            handleoptionschange={this.handleOptionsChange}
                             setdefaultvalues={this.setDefaultValues}
                             title={this.state.title}
                             exportfilename={this.state.exportFileName}
                             interactivityenabled={this.state.interactivityEnabled ? 1 : 0}
-                            themes={[{value: "light1", content: "Light1"}, {value: "light2", content: "Light2"}, {value: "dark1", content: "Dark1"}, {value: "dark2", content: "Dark2"}]}
+                            themes={[{ value: "light1", content: "Light1" }, { value: "light2", content: "Light2" }, { value: "dark1", content: "Dark1" }, { value: "dark2", content: "Dark2" }]}
                             currenttheme={this.state.theme}
-                            types={[{type: "column", name: "Stulpelinė"}, {type: "pie", name: "Skritulinė"}]}
+                            types={[{ type: "column", name: "Stulpelinė" }, { type: "pie", name: "Skritulinė" }]}
                             currenttype={this.state.type}
                             axisxtitle={this.state.axisXTitle}
                             axisxlabelangle={this.state.axisXLabelAngle}
@@ -222,6 +296,22 @@ export class GrandSlams extends Component {
                             currenttitlefont={this.state.titleFont}
                             currentaxisxfont={this.state.axisXFont}
                             currentaxisyfont={this.state.axisYFont}
+                        />
+                        <br />
+                        <br />
+                        <Button variant="primary" onClick={() => this.setState({ dataOptionsModalShow: true })}>
+                            Keisti duomenų parinktis
+                        </Button>
+                        <DataOptionsModal
+                            animation={false}
+                            size="lg"
+                            show={this.state.dataOptionsModalShow}
+                            onHide={() => this.setState({ dataOptionsModalShow: false })}
+                            handleoptionschange={this.handleOptionsChange}
+                            setdefaultdatafilters={this.setDefaultDataFilters}
+                            from={this.state.from}
+                            to={this.state.to !== '' ? this.state.to : Math.max.apply(Math, this.state.grandSlams.map(grandSlam => grandSlam.grandSlamCount))}
+                            selectednationalities={this.state.selectedNationalities}
                         />
                         <br />
                         <br />
