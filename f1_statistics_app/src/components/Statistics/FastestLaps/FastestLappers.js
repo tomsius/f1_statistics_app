eb63d3de605654a22022e938ac9277d8f0f342ea
Tomas Kašelynas <tomaskas98@gmail.com>
2021-04-08 10:48:24 +0300

    Refactoring

--- f1_statistics_app/src/components/Statistics/FastestLaps/FastestLappers.js
+++ f1_statistics_app/src/components/Statistics/FastestLaps/FastestLappers.js
@@ -34,7 +34,7 @@ export class FastestLappers extends Component {
             axisYMinimum: 0,
             axisYMaximum: '',
             axisYInterval: 5,
-            
+
             titleFont: "Calibri",
             axisXFont: "Calibri",
             axisYFont: "Calibri",
@@ -67,7 +67,7 @@ export class FastestLappers extends Component {
         var totalFastest = 0;
 
         fastestLappers.forEach(fastestLapper => {
-            totalFastest += fastestLapper.fastestLapsCount
+            totalFastest += fastestLapper.totalFastestLapsCount
         });
 
         return totalFastest;
@@ -111,7 +111,7 @@ export class FastestLappers extends Component {
             axisYMinimum: 0,
             axisYMaximum: '',
             axisYInterval: 5,
-            
+
             titleFont: "Calibri",
             axisXFont: "Calibri",
             axisYFont: "Calibri"
@@ -138,59 +138,71 @@ export class FastestLappers extends Component {
 
         this.state.selectedCircuits.forEach(selectedCircuit => {
             if (selectedCircuit.checked === false) {
-                filteredData.forEach(x => {
-                    var i = 0;
+                filteredData.forEach(fastestLapper => {
+                    fastestLapper.fastestLapsByYear.forEach(year => {
+                        var i = 0;
 
-                    while (i < x.fastestLapInformation.length) {
-                        if (x.fastestLapInformation[i].circuitName === selectedCircuit.circuit) {
-                            x.fastestLapInformation.splice(i, 1);
+                        while (i < year.fastestLapInformation.length) {
+                            if (year.fastestLapInformation[i].circuitName === selectedCircuit.circuit) {
+                                year.fastestLapInformation.splice(i, 1);
+                            }
+                            else {
+                                i++;
+                            }
                         }
-                        else {
-                            i++;
-                        }
-                    }
 
-                    x.fastestLapsCount = x.fastestLapInformation.length;
+                        year.yearFastestLapCount = year.fastestLapInformation.length;
+                    });
+
+                    fastestLapper.totalFastestLapsCount = fastestLapper.fastestLapsByYear.map(year => year.yearFastestLapCount).reduce((accumulator, currentValue) => accumulator + currentValue);
                 });
             }
         });
-        
-        filteredData.forEach(x => {
-            var i = 0;
 
-            while (i < x.fastestLapInformation.length) {
-                if (x.fastestLapInformation[i].gridPosition < this.state.gridFrom) {
-                    x.fastestLapInformation.splice(i, 1);
-                }
-                else {
-                    i++;
-                }
-            }
-
-            x.fastestLapsCount = x.fastestLapInformation.length;
-        });
-
-        if (this.state.gridTo !== '') {
-            filteredData.forEach(x => {
+        filteredData.forEach(fastestLapper => {
+            fastestLapper.fastestLapsByYear.forEach(year => {
                 var i = 0;
 
-                while (i < x.fastestLapInformation.length) {
-                    if (x.fastestLapInformation[i].gridPosition > this.state.gridTo) {
-                        x.fastestLapInformation.splice(i, 1);
+                while (i < year.fastestLapInformation.length) {
+                    if (year.fastestLapInformation[i].gridPosition < this.state.gridFrom) {
+                        year.fastestLapInformation.splice(i, 1);
                     }
                     else {
                         i++;
                     }
                 }
 
-                x.fastestLapsCount = x.fastestLapInformation.length;
+                year.yearFastestLapCount = year.fastestLapInformation.length;
+            });
+
+            fastestLapper.totalFastestLapsCount = fastestLapper.fastestLapsByYear.map(year => year.yearFastestLapCount).reduce((accumulator, currentValue) => accumulator + currentValue);
+        });
+
+        if (this.state.gridTo !== '') {
+            filteredData.forEach(fastestLapper => {
+                fastestLapper.fastestLapsByYear.forEach(year => {
+                    var i = 0;
+
+                    while (i < year.fastestLapInformation.length) {
+                        if (year.fastestLapInformation[i].gridPosition > this.state.gridTo) {
+                            year.fastestLapInformation.splice(i, 1);
+                        }
+                        else {
+                            i++;
+                        }
+                    }
+
+                    year.yearFastestLapCount = year.fastestLapInformation.length;
+                });
+
+                fastestLapper.totalFastestLapsCount = fastestLapper.fastestLapsByYear.map(year => year.yearFastestLapCount).reduce((accumulator, currentValue) => accumulator + currentValue);
             });
         }
-        
-        filteredData = filteredData.filter(x => x.fastestLapsCount >= this.state.from);
+
+        filteredData = filteredData.filter(x => x.totalFastestLapsCount >= this.state.from);
 
         if (this.state.to !== '') {
-            filteredData = filteredData.filter(x => x.fastestLapsCount <= this.state.to);
+            filteredData = filteredData.filter(x => x.totalFastestLapsCount <= this.state.to);
         }
 
         return filteredData;
@@ -199,9 +211,11 @@ export class FastestLappers extends Component {
     getCircuits(data) {
         var uniqueCircuits = new Set();
 
-        data.forEach(x => {
-            x.fastestLapInformation.forEach(y => {
-                uniqueCircuits.add(y.circuitName);
+        data.forEach(fastestLapper => {
+            fastestLapper.fastestLapsByYear.forEach(year => {
+                year.fastestLapInformation.forEach(information => {
+                    uniqueCircuits.add(information.circuitName);
+                });
             });
         });
 
@@ -245,10 +259,11 @@ export class FastestLappers extends Component {
 
     render() {
         if (this.state.fastestLappers.length > 0) {
+            var filteredData = this.filterData(this.state.fastestLappers);
+
             if (this.state.type !== "stackedColumn") {
-                var filteredData = this.filterData(this.state.fastestLappers);
                 var totalFastest = this.calculateTotalFastest(filteredData);
-                var data = filteredData.map((x, index) => ({ label: x.name, x: index + 1, y: x.fastestLapsCount, percentage: Math.round((x.fastestLapsCount / totalFastest * 100) * 100) / 100 }));
+                var data = filteredData.map((x, index) => ({ label: x.name, x: index + 1, y: x.totalFastestLapsCount, percentage: Math.round((x.totalFastestLapsCount / totalFastest * 100) * 100) / 100 }));
 
                 if (this.state.axisYMaximum === '') {
                     var defaultMaximum = -1;
@@ -262,7 +277,7 @@ export class FastestLappers extends Component {
                 }
             }
             else {
-                var data = this.state.fastestLappers.map(x => ({ type: "stackedColumn", showInLegend: true, name: x.name, dataPoints: x.fastestLapsByYear.map(yearFastestLap => ({ label: yearFastestLap.year, x: yearFastestLap.year, y: yearFastestLap.fastestLapCount })) }));
+                var data = filteredData.map(x => ({ type: "stackedColumn", showInLegend: true, name: x.name, dataPoints: x.fastestLapsByYear.map(yearFastestLap => ({ label: yearFastestLap.year, x: yearFastestLap.year, y: yearFastestLap.yearFastestLapCount })) }));
 
                 defaultMaximum = 25;
             }
@@ -314,8 +329,10 @@ export class FastestLappers extends Component {
                         var content = e.entries[0].dataPoint.x + "<br />";
                         var total = 0;
                         for (let i = 0; i < e.entries.length; i++) {
-                            content += e.entries[i].dataSeries.name + ": " + e.entries[i].dataPoint.y + "<br />";
-                            total += e.entries[i].dataPoint.y;
+                            if (e.entries[i].dataPoint.y > 0) {
+                                content += e.entries[i].dataSeries.name + ": " + e.entries[i].dataPoint.y + "<br />";
+                                total += e.entries[i].dataPoint.y;
+                            }
                         }
                         content += "Iš viso: " + total;
                         return content;
@@ -350,12 +367,12 @@ export class FastestLappers extends Component {
                             interactivityenabled={this.state.interactivityEnabled ? 1 : 0}
                             themes={[{ value: "light1", content: "Light1" }, { value: "light2", content: "Light2" }, { value: "dark1", content: "Dark1" }, { value: "dark2", content: "Dark2" }]}
                             currenttheme={this.state.theme}
-                            types={[{ type: "column", name: "Stulpelinė" }, {type: "stackedColumn", name: "Stulpelinė (sluoksniuota)"}, { type: "pie", name: "Skritulinė" }]}
+                            types={[{ type: "column", name: "Stulpelinė" }, { type: "stackedColumn", name: "Stulpelinė (sluoksniuota)" }, { type: "pie", name: "Skritulinė" }]}
                             currenttype={this.state.type}
-                            axisxtitle={this.state.axisXTitle}
+                            axisxtitle={this.state.type !== "stackedColumn" ? this.state.axisXTitle : this.state.axisXTitle2}
                             axisxlabelangle={this.state.axisXLabelAngle}
                             axisxgridthickness={this.state.axisXGridThickness}
-                            axisxtitle={this.state.type !== "stackedColumn" ? this.state.axisXTitle : this.state.axisXTitle2}
+                            axisytitle={this.state.axisYTitle}
                             axisylabelangle={this.state.axisYLabelAngle}
                             axisygridthickness={this.state.axisYGridThickness}
                             axisyminimum={this.state.axisYMinimum}
@@ -379,9 +396,9 @@ export class FastestLappers extends Component {
                             handleoptionschange={this.handleOptionsChange}
                             setdefaultdatafilters={this.setDefaultDataFilters}
                             from={this.state.from}
-                            to={this.state.to !== '' ? this.state.to : Math.max.apply(Math, this.state.fastestLappers.map(x => x.fastestLapsCount))}
+                            to={this.state.to !== '' ? this.state.to : Math.max.apply(Math, this.state.fastestLappers.map(x => x.totalFastestLapsCount))}
                             gridfrom={this.state.gridFrom}
-                            gridto={this.state.gridTo !== '' ? this.state.gridTo : Math.max.apply(Math, this.state.fastestLappers.map(x => Math.max.apply(Math, x.fastestLapInformation.map(y => y.gridPosition))))}
+                            gridto={this.state.gridTo !== '' ? this.state.gridTo : Math.max.apply(Math, this.state.fastestLappers.map(fastestLapper => fastestLapper.fastestLapsByYear.map(year => Math.max.apply(Math, year.fastestLapInformation.map(information => information.gridPosition)))))}
                             selectedcircuits={this.state.selectedCircuits}
                         />
                         <br />
